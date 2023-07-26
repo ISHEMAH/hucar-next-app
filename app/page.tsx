ffb70aba9af6e0e4202d49d218136224e9c17c62
ISHEMAH <121429065+ISHEMAH@users.noreply.github.com>
2023-07-26 23:51:32 +0200

    finished fetching

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
       <Hero />
       <div className="mt-12 padding-x padding-y max-width" id="discover">
         <div className="home__text-container">
-          <h1 className="tezt-4xl font-extrabold">Car Catalogue</h1>
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
           <p>Explore the cars you might like</p>
         </div>
         <div className="home__filters">
